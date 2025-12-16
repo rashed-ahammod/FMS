d2ee16529329303b463aeee490a52cbcbdfa41aa
Nabil Nasim <nabilmohammednasim@gmail.com>
2025-12-17 00:37:50 +0600

    form, body,updated. linker added

--- Login.php
+++ Login.php
@@ -18,7 +18,7 @@
         border-radius: 10px;
         width: 500px;
         height: 500px;
-        margin: auto;
+        margin: 200px auto 0 auto;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         
     }
@@ -27,7 +27,7 @@
     {
         width: 95%;
         padding: 10px;
-        margin-top: 10px;
+        margin-top: 15px;
         border: 1px solid #ccc;
         border-radius: 5px;
     }
@@ -37,18 +37,26 @@
         padding: 12px;
         width: 100%;
     }
+
+    .links
+    {
+        text-align: center;
+        margin-top: 20px;
+    }
+
     
 </style>
 <body>
-    <center>
-    <h1>Login</h1>
-    </center>
 
     <form onsubmit="return handlesubmit()">
         <h1 style="text-align: center;">Sign in with your email</h1>
         Email:<br>
-        <input type="email" id="email" name="email" required><br><br>
+        <input type="email" id="email" name="email" required placeholder="Please write your registered email"><br><br>
         Password:<br>
-        <input type="password" id="password" name="password" required><br><br>
+        <input type="password" id="password" name="password" required placeholder="Please write your password"><br><br><br><br>
         <button type="submit">Login</button>
+
+        <div class="links">
+            <a href="">Not a user? Sign Up</a><br>
+            <a href="">Forgot Password?</a><br>
     </form>
