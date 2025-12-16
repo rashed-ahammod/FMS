6800d7d21abdab7171d8f4b887d3da1a99d5a92f
Nabil Nasim <nabilmohammednasim@gmail.com>
2025-12-17 00:23:17 +0600

    login css updated

--- Login.php
+++ Login.php
@@ -16,12 +16,28 @@
         background-color: #ffffff;
         padding:20px;
         border-radius: 10px;
-        width: 450px;
+        width: 500px;
         height: 500px;
         margin: auto;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         
     }
+
+    input
+    {
+        width: 95%;
+        padding: 10px;
+        margin-top: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+    
+    button
+    {
+        padding: 12px;
+        width: 100%;
+    }
+    
 </style>
 <body>
     <center>
@@ -29,9 +45,10 @@
     </center>
 
     <form onsubmit="return handlesubmit()">
-        Username:<br>
-        <input type="text" id="username" name="username" required><br><br>
+        <h1 style="text-align: center;">Sign in with your email</h1>
+        Email:<br>
+        <input type="email" id="email" name="email" required><br><br>
         Password:<br>
         <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Login">
+        <button type="submit">Login</button>
     </form>
