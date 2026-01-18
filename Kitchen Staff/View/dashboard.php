65730b0ca82ca9b5281b10463d42169ce289d083
rashed-ahammod <ahammodr74@gmail.com>
2026-01-18 21:15:08 +0600

    dashboard_feature

---
+++ Kitchen Staff/View/dashboard.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kitchen Staff Dashboard</title>
+    <link rel="stylesheet" href="../CSS/dashboard.css">
+
+
+</head>
+<body>
+    <div class="dashboard_header">
+        <div class="logo">Online Food Service Management</div>
+        <div class="profile">
+            <span class="profile_name">Profile</span>
+            <div class="options">
+                <a href="profile.php">My Profile</a>
+                <a href="">Change Password</a>
+                <a href="">Logout</a>
+</div>
+</div>
+</div>
+<div class="dashboard">
+    <h1>Kitchen Staff Dashboard<h1>
+        <div class="feature_list">
+            <a href="../View/manage_menu.php" class ="feature">
+            <h2>Manage Menu</h2>
+                <p>Add,Update or Remove Menu Items</p>
+</a>
+        <a href="../View/order_tracking.php" class ="feature">
+            <h2>Update Order Status</h2>
+                <p>Track and Update Order Status</p>
+</a>
+        <a href="../View/orderHistory.php" class ="feature">
+            <h2>Order History</h2>
+                <p>All Completed Order History</p>
+</a>
+        <a href="" class ="feature">
+            <h2>Availability</h2>
+                <p>Set Food item wether availabe or unavailabe</p>
+</a>
+        <a href="" class ="feature">
+            <h2>Review</h2>
+                <p>Customer Feedback</p>
+</a>
+</div>
+</div>
+</body>
+</html>
+
