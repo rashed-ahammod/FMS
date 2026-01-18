7136a9bb3a50825614d4105971697e1f5e347e96
rashed-ahammod <ahammodr74@gmail.com>
2026-01-18 21:23:36 +0600

    order_tracking  feature

---
+++ Kitchen Staff/Controller/order_ttrackingController.php
@@ -0,0 +1,46 @@
+<?php
+require_once '../Model/order_trackingModel.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+
+  $data = json_decode(file_get_contents("php://input"), true);
+if (!isset($data['order_id'], $data['order_status'])) {
+    header("Content-Type: application/json");
+    echo json_encode([
+        "success" => false,
+       // "message" => "Invalid JSON data"
+    ]);
+    exit;
+}
+
+$order_id = $data['order_id'];
+$status   = $data['order_status'];
+
+if (empty($order_id) || empty($status)) {
+    echo json_encode([
+        "success" => false,
+        "message" => "Order ID or status missing"
+    ]);
+    exit;
+}
+
+$result = updateOrderStatus($order_id, $status);
+header("Content-Type: application/json");
+if ($result) {
+    echo json_encode([
+        "success" => $result,
+       // "message" => "Order status updated successfully",
+        "order_id" => $order_id,
+        "order_status" => $status
+    ]);
+} else {
+    echo json_encode([
+        "success" => false,
+       // "message" => "Failed to update order status"
+    ]);
+}
+exit;
+}
+?>
---
+++ Kitchen Staff/Model/order_trackingModel.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'connection.php';
+
+function getKitchenOrders() {
+    $conn=getConnection();
+     $sql = "
+  SELECT 
+    o.order_id,
+    o.order_status,
+    m.name AS food_name,
+    oi.quantity
+FROM orders o
+LEFT JOIN order_item oi ON o.order_id = oi.order_id
+LEFT JOIN menu m ON oi.menu_id = m.menu_id
+WHERE o.order_status != 'Delivered'
+ORDER BY o.order_time ASC
+";
+
+    return mysqli_query($conn, $sql);
+}
+
+function updateOrderStatus($order_id, $status) {
+    $conn = getConnection();
+
+    $sql = "UPDATE orders SET order_status='$status' WHERE order_id='$order_id'";
+    return mysqli_query($conn, $sql);
+}
+
