2a142b9cd1a0414626e06fa6dd1f78d255b7f3ea
Nabil Nasim <nabilmohammednasim@gmail.com>
2026-01-19 18:50:57 +0600

    cookie for customer done

--- Customer/View/Login.php
+++ Customer/View/Login.php
@@ -24,11 +24,14 @@ include '../Controller Logic/logincontroller.php';
         Password:<br>
         <input type="password" id="password" name="password" required placeholder="Please write your password">
         <?php echo $passErr; ?>
-           <br><br>
-            <input type="checkbox" name="remember_me"
-        <?php if(isset($_COOKIE['remember_email'])) echo "checked"; ?>> Remember Me
+           
+        <div class="remember-box">
+        <input type="checkbox" name="remember_me"
+        <?php echo isset($_COOKIE['remember_email']) ? 'checked' : ''; ?>>
+        <label>Remember Me</label>
+        </div>
         
-        <br><br>
+        <br>
         <button type="submit" name="Login_btn">Login</button>
 
         <div class="links">
